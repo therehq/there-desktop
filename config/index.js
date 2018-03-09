b963013be546eef63144007e756c11f5825353bf
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-09 14:09:20 +0330

    Adopt api url based on the environment

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
+const isDev = require('electron-is-dev')
+const host = isDev ? 'http://localhost:9900' : 'https://api.there.pm'
+
 module.exports = {
   devPort: 8008,
   crispWebsiteId: `bb14ccd2-0869-40e7-b0f1-b520e93db7e1`,
-  apiUrl: 'https://api.there.pm',
-  graphqlEndpoint: 'https://api.there.pm/graphql',
+  apiUrl: host,
+  graphqlEndpoint: `${host}/graphql`,
 }
