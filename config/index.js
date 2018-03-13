3407a4c05f5f78b576403a13e70e4f22b0b9e7a2
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-13 12:12:56 +0330

    Add a flag to enable using online API or local

--- config/index.js
+++ config/index.js
@@ -1,6 +1,15 @@
-// const isDev = require('electron-is-dev')
-// const host = isDev ? 'http://localhost:9900' : 'https://api.there.pm'
-const host = 'https://api.there.pm'
+const isDev = require('electron-is-dev')
+
+let host
+if (process.env.ONLINE_API === '1') {
+  host = 'https://api.there.pm'
+} else if (process.env.ONLINE_API === '0') {
+  host = 'http://localhost:9900'
+} else {
+  host = isDev ? 'http://localhost:9900' : 'https://api.there.pm'
+}
+
+console.log('Connecting to server at:', host)
 
 module.exports = {
   devPort: 8008,
