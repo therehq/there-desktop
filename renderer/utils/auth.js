bf0c0a3f7c3ee93b18c9702d95acdec8cfa7ff33
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-18 18:35:47 +0330

    Fix isLoggedIn

--- renderer/utils/auth.js
+++ renderer/utils/auth.js
@@ -1,5 +1,5 @@
 // Local
 import { getToken } from './store'
 
-export const isLoggedIn = () => getToken() !== null
+export const isLoggedIn = () => Boolean(getToken())
 export { getToken, setToken, setUser, getUser, setUserAndToken } from './store'
