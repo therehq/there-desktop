a423e4bc5383028ab85d6f17965dc5feb58171d5
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-22 16:52:59 +0430

    Add copy/paste and devtools

--- config/index.js
+++ config/index.js
@@ -9,11 +9,13 @@ if (process.env.ONLINE_API === '1') {
   host = isDev ? 'http://localhost:9900' : 'https://api.there.pm'
 }
 
-console.log('Connecting to server at:', host)
+console.log(`🔥 Connecting to server at: ${host}`)
 
 module.exports = {
   devPort: 8008,
-  crispWebsiteId: `bb14ccd2-0869-40e7-b0f1-b520e93db7e1`,
   apiUrl: host,
   graphqlEndpoint: `${host}/graphql`,
+  crispWebsiteId: `bb14ccd2-0869-40e7-b0f1-b520e93db7e1`,
+  sentryDSN: `https://2659a1e4dfb14debb5efd5f2626d69b9@sentry.io/287684`,
+  GATrackingId: `UA-116027138-1`,
 }
