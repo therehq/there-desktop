7a6fc9046eb16782de4f55f3c07ed0dd4b0f0c17
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-25 17:06:07 +0430

    Remove fadeDown animation for places

--- renderer/components/tray/Group/Content.js
+++ renderer/components/tray/Group/Content.js
@@ -5,29 +5,18 @@ const fadeUp = keyframes`
   to { opacity: 0.5; transform: translateY(-10px); }
 `
 
-const fadeDown = keyframes`
-  from { opacity: 0.5; transform: translateY(-10px); }
-  to { opacity: 1; transform: translateY(0); }
-`
-
 const Content = styled.div`
   overflow: hidden;
   position: relative;
   width: 100%;
 
   ${p =>
-    p.collapsed === true
-      ? css`
-          > div > * {
-            animation: ${fadeUp} 100ms ease;
-          }
-        `
-      : p.collapsed === false &&
-        css`
-          > div > * {
-            animation: ${fadeDown} 100ms ease;
-          }
-        `};
+    p.collapsed === true &&
+    css`
+      > div > * {
+        animation: ${fadeUp} 100ms ease;
+      }
+    `};
 `
 
 export default Content
