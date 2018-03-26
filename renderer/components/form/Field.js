cb424f1e4eb5fa23a45a969ec6df4472b15547be
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-26 19:18:01 +0430

    Add animation to field wrapper

--- renderer/components/form/Field.js
+++ renderer/components/form/Field.js
@@ -1,5 +1,16 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+  from { opacity: 0; }
+  to { opacity: 1; }
+`
 
 export const FieldWrapper = styled.div`
   margin-top: ${p => (p.moreTop ? '22px' : '12px')};
+
+  ${p =>
+    p.animation &&
+    css`
+      animation: ${fadeIn} 100ms ${p.animationDelay} backwards ease-out;
+    `};
 `
