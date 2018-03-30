44939707124fd15bda82b420708c44565edbba02
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-30 16:15:04 +0430

    Remove top space on Windows

--- renderer/components/Popover.js
+++ renderer/components/Popover.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import TopArrowPosition from './tray/TopArrowPosition'
 import Caret from '../vectors/Caret'
 
-const isWindows = process.platform === 'win32'
+const notWindows = process.platform !== 'win32'
 
 const PopoverBox = styled.div`
   height: 100%;
@@ -29,14 +29,14 @@ const TransparentWrapper = styled.div`
   background: transparent;
   height: 100vh;
   width: 100vw;
-  padding-top: 14px;
+  padding-top: ${p => (p.arrowSpace ? 14 : 0)}px;
   box-sizing: border-box;
 `
 
 const Popover = ({ children, ...props }) => (
-  <TransparentWrapper {...props}>
+  <TransparentWrapper {...props} arrowSpace={notWindows}>
     <PopoverBox>
-      {isWindows || (
+      {notWindows && (
         <TopArrowPosition>
           {left => (
             <TopArrowWrapper left={left}>
