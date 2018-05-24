4e9b4d53922dbf08180c71f5e2c0fb9d9e16f475
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-05-25 01:25:25 +0430

    Smaller description texts

--- renderer/components/window/Desc.js
+++ renderer/components/window/Desc.js
@@ -1,13 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Desc = styled.p`
-  line-height: 1.3;
+  line-height: 1.4;
   font-size: 15px;
   font-weight: normal;
   margin: 5px auto 10px auto;
   max-width: 430px;
 
   color: ${p => p.theme.colors.grayText};
+
+  ${p =>
+    p.fullWidth &&
+    css`
+      max-width: 100%;
+      margin-right: 0;
+      margin-left: 0;
+    `};
 `
 
 export default Desc
