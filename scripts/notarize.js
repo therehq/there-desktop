a6e6ef1c8f5cde698d146319cf2be43c183d5a0e
Mo <mo@there.team>
2019-10-16 13:48:40 +0330

    Fix for Catalina (Notarize)

---
+++ scripts/notarize.js
@@ -0,0 +1,40 @@
+/**
+ * Source: https://github.com/electron-userland/electron-builder/issues/3870#issuecomment-498786448
+ * Source: https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
+ */
+
+require('dotenv').config()
+
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context
+
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  if (!(process.env.APPLE_ID && process.env.APPLE_ID_PASSWORD)) {
+    console.warn(
+      'Skipping macOS app notarization.' +
+        ' Missing one or more environment vars (APPLE_ID, APPLE_ID_PASSWORD).'
+    )
+    return
+  }
+
+  if (process.env.DEV_BUILD) {
+    console.warn(
+      'Skipping macOS app notarization in development build. Do not publish'
+    )
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return await notarize({
+    appBundleId: 'pm.there.desktop',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  })
+}
